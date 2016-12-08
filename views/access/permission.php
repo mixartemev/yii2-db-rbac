17eb5ac6a37da6c0d163d3ef5e9bd0afa7d98234
Mikhail <mixartemev@gmail.com>
2016-12-08 18:17:28 +0300

    tree

--- views/access/permission.php
+++ views/access/permission.php
@@ -1,10 +1,10 @@
 <?php
 namespace mixartemev\db_rbac\views\access;
 
+use common\components\RbacManager;
+use leandrogehlen\treegrid\TreeGrid;
 use Yii;
 use yii\data\ArrayDataProvider;
-use yii\grid\GridView;
-use yii\grid\DataColumn;
 use yii\helpers\Url;
 use yii\helpers\Html;
 
@@ -19,51 +19,50 @@ $this->params['breadcrumbs'][] = $this->title;
         <?= Html::a(Yii::t('db_rbac', 'Добавить новое правило'), ['add-permission'], ['class' => 'btn btn-success']) ?>
     </p>
 <?php
-$dataProvider = new ArrayDataProvider([
-      'allModels' => Yii::$app->authManager->getPermissions(),
-      'sort' => [
-          'attributes' => ['name', 'description'],
-      ],
-      'pagination' => [
-          'pageSize' => 10,
-      ],
- ]);
-?>
+/** @var RbacManager $auth */
+$auth = Yii::$app->authManager;
 
-<?=GridView::widget([
+$dataProvider = new ArrayDataProvider([
+    'allModels' => $auth->allItems(),
+    'sort' => [
+        'attributes' => ['name', 'description'],
+    ],
+    'pagination' => [
+        'pageSize' => 50,
+    ]
+]);
+?>
+<?= TreeGrid::widget([
     'dataProvider' => $dataProvider,
+    'keyColumnName' => 'name',
+    'parentColumnName' => 'parent',
+    'parentRootValue' => null, //first parentId value
+    'pluginOptions' => [
+        //'initialState' => 'collapsed',
+    ],
     'columns' => [
-        ['class' => 'yii\grid\SerialColumn'],
-        [
-            'class'     => DataColumn::className(),
-            'attribute' => 'name',
-            'label'     => Yii::t('db_rbac', 'Правило')
-        ],
-        [
-            'class'     => DataColumn::className(),
-            'attribute' => 'description',
-            'label'     => Yii::t('db_rbac', 'Описание')
-        ],
+        ['attribute' => 'name', 'header' => 'Разрешение'/*, 'headerOptions' => ['width' => '35%']*/],
+        ['attribute' => 'description', 'header' => 'Описание'],
+        ['attribute' => 'parent', 'header' => 'Родитель'],
         ['class' => 'yii\grid\ActionColumn',
             'template' => '{update} {delete}',
             'buttons' =>
                 [
                     'update' => function ($url, $model) {
-                                return Html::a('<span class="glyphicon glyphicon-pencil"></span>', Url::toRoute(['update-permission', 'name' => $model->name]), [
-                                        'title' => Yii::t('yii', 'Update'),
-                                        'data-pjax' => '0',
-                                    ]); },
+                        return Html::a('<span class="glyphicon glyphicon-pencil"></span>', Url::toRoute(['update-permission', 'name' => $model['name']]), [
+                            'title' => Yii::t('yii', 'Update'),
+                            'data-pjax' => '0',
+                        ]); },
                     'delete' => function ($url, $model) {
-                                return Html::a('<span class="glyphicon glyphicon-trash"></span>', Url::toRoute(['delete-permission','name' => $model->name]), [
-                                        'title' => Yii::t('yii', 'Delete'),
-                                        'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
-                                        'data-method' => 'post',
-                                        'data-pjax' => '0',
-                                    ]);
-                        }
+                        return Html::a('<span class="glyphicon glyphicon-trash"></span>', Url::toRoute(['delete-permission','name' => $model['name']]), [
+                            'title' => Yii::t('yii', 'Delete'),
+                            'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+                            'data-method' => 'post',
+                            'data-pjax' => '0',
+                        ]);
+                    }
                 ]
-        ],
         ]
-    ]);
-?>
+    ]
+]) ?>
 </div>
