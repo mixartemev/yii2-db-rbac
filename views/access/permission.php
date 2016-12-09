79dcf7c4a47050145dd02fd220a9b804ff0f6282
Mikhail <mixartemev@gmail.com>
2016-12-09 13:07:18 +0300

    Update permission.php

--- views/access/permission.php
+++ views/access/permission.php
@@ -8,61 +8,60 @@ use yii\data\ArrayDataProvider;
 use yii\helpers\Url;
 use yii\helpers\Html;
 
-$this->title = Yii::t('db_rbac', 'Правила доступа');
+$this->title = Yii::t('db_rbac', 'Разрешения');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="news-index">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
+<div class="box box-default">
+    <div class="box-body">
     <p>
         <?= Html::a(Yii::t('db_rbac', 'Добавить новое правило'), ['add-permission'], ['class' => 'btn btn-success']) ?>
     </p>
-    <?php
-    /** @var RbacManager $auth */
-    $auth = Yii::$app->authManager;
+<?php
+/** @var RbacManager $auth */
+$auth = Yii::$app->authManager;
 
-    $dataProvider = new ArrayDataProvider([
-        'allModels' => $auth->allItems(),
-        'sort' => [
-            'attributes' => ['name', 'description'],
-        ],
-        'pagination' => [
-            'pageSize' => 50,
+$dataProvider = new ArrayDataProvider([
+    'allModels' => $auth->allItems(),
+    'sort' => [
+        'attributes' => ['name', 'description'],
+    ],
+    'pagination' => [
+        'pageSize' => 50,
+    ]
+]);
+?>
+<?= TreeGrid::widget([
+    'dataProvider' => $dataProvider,
+    'keyColumnName' => 'name',
+    'parentColumnName' => 'parent',
+    'parentRootValue' => null, //first parentId value
+    'pluginOptions' => [
+        //'initialState' => 'collapsed',
+    ],
+    'columns' => [
+        ['attribute' => 'name', 'header' => 'Разрешение'/*, 'headerOptions' => ['width' => '35%']*/],
+        ['attribute' => 'description', 'header' => 'Описание'],
+        ['attribute' => 'parent', 'header' => 'Родитель'],
+        ['class' => 'yii\grid\ActionColumn',
+            'template' => '{update} {delete}',
+            'buttons' =>
+                [
+                    'update' => function ($url, $model) {
+                        return Html::a('<span class="glyphicon glyphicon-pencil"></span>', Url::toRoute(['update-permission', 'name' => $model['name']]), [
+                            'title' => Yii::t('yii', 'Update'),
+                            'data-pjax' => '0',
+                        ]); },
+                    'delete' => function ($url, $model) {
+                        return Html::a('<span class="glyphicon glyphicon-trash"></span>', Url::toRoute(['delete-permission','name' => $model['name']]), [
+                            'title' => Yii::t('yii', 'Delete'),
+                            'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+                            'data-method' => 'post',
+                            'data-pjax' => '0',
+                        ]);
+                    }
+                ]
         ]
-    ]);
-    ?>
-    <?= TreeGrid::widget([
-        'dataProvider' => $dataProvider,
-        'keyColumnName' => 'name',
-        'parentColumnName' => 'parent',
-        'parentRootValue' => null, //first parentId value
-        'pluginOptions' => [
-            //'initialState' => 'collapsed',
-        ],
-        'columns' => [
-            ['attribute' => 'name', 'header' => 'Разрешение'/*, 'headerOptions' => ['width' => '35%']*/],
-            ['attribute' => 'description', 'header' => 'Описание'],
-            ['attribute' => 'parent', 'header' => 'Родитель'],
-            ['class' => 'yii\grid\ActionColumn',
-                'template' => '{update} {delete}',
-                'buttons' =>
-                    [
-                        'update' => function ($url, $model) {
-                            return Html::a('<span class="glyphicon glyphicon-pencil"></span>', Url::toRoute(['update-permission', 'name' => $model['name']]), [
-                                'title' => Yii::t('yii', 'Update'),
-                                'data-pjax' => '0',
-                            ]); },
-                        'delete' => function ($url, $model) {
-                            return Html::a('<span class="glyphicon glyphicon-trash"></span>', Url::toRoute(['delete-permission','name' => $model['name']]), [
-                                'title' => Yii::t('yii', 'Delete'),
-                                'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
-                                'data-method' => 'post',
-                                'data-pjax' => '0',
-                            ]);
-                        }
-                    ]
-            ]
-        ]
-    ]) ?>
+    ]
+]) ?>
+    </div>
 </div>
