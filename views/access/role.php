1996e722976f46c9325c4fd17bf132ad5384cab4
Mikhail <mixartemev@gmail.com>
2016-12-09 13:07:48 +0300

    Update role.php

--- views/access/role.php
+++ views/access/role.php
@@ -1,6 +1,9 @@
 <?php
+
 namespace mixartemev\db_rbac\views\access;
 
+use common\components\RbacManager;
+use leandrogehlen\treegrid\TreeGrid;
 use Yii;
 use yii\data\ArrayDataProvider;
 use yii\grid\GridView;
@@ -8,69 +11,80 @@ use yii\grid\DataColumn;
 use yii\helpers\Url;
 use yii\helpers\Html;
 use yii\helpers\ArrayHelper;
+use yii\rbac\Item;
+
+/* @var array $roles all */
 
 $this->title = Yii::t('db_rbac', 'Управление ролями');
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="news-index">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
+<div class="box box-default">
+    <div class="box-body">
     <p>
         <?= Html::a(Yii::t('db_rbac', 'Добавить роль'), ['add-role'], ['class' => 'btn btn-success']) ?>
     </p>
-<?php
-$dataProvider = new ArrayDataProvider([
-      'allModels' => Yii::$app->authManager->getRoles(),
-      'sort' => [
-          'attributes' => ['name', 'description'],
-      ],
-      'pagination' => [
-          'pageSize' => 10,
-      ],
- ]);
-?>
-
-<?=GridView::widget([
-    'dataProvider' => $dataProvider,
-    'columns' => [
-        ['class' => 'yii\grid\SerialColumn'],
-        [
-            'class'     => DataColumn::className(),
-            'attribute' => 'name',
-            'label'     => Yii::t('db_rbac', 'Роль')
-        ],
-        [
-            'class'     => DataColumn::className(),
-            'attribute' => 'description',
-            'label'     => Yii::t('db_rbac', 'Описание')
-        ],
-        [
-            'class'     => DataColumn::className(),
-            'label'     => Yii::t('db_rbac', 'Разрешенные доступы'),
-            'format'    => ['html'],
-            'value'     => function($data) { return implode('<br>',array_keys(ArrayHelper::map(Yii::$app->authManager->getPermissionsByRole($data->name), 'description', 'description')));}
-        ],
-        ['class' => 'yii\grid\ActionColumn',
-            'template' => '{update} {delete}',
-            'buttons' =>
-                [
-                    'update' => function ($url, $model) {
-                                return Html::a('<span class="glyphicon glyphicon-pencil"></span>', Url::toRoute(['update-role', 'name' => $model->name]), [
-                                        'title' => Yii::t('yii', 'Update'),
-                                        'data-pjax' => '0',
-                                    ]); },
-                    'delete' => function ($url, $model) {
-                                return Html::a('<span class="glyphicon glyphicon-trash"></span>', Url::toRoute(['delete-role','name' => $model->name]), [
-                                        'title' => Yii::t('yii', 'Delete'),
-                                        'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
-                                        'data-method' => 'post',
-                                        'data-pjax' => '0',
-                                    ]);
-                        }
-                ]
-        ],
-        ]
-    ]);
-?>
+        <?php
+        /** @var RbacManager $auth */
+        $auth = Yii::$app->authManager;
+        /** массив всех родственных связей AuthItem-ов */
+        $childList = $auth->childrenRowList();
+        /** получаем из него Adjacency List ролей */
+        $adjacencyListRoles = [];
+        foreach($roles as $name => $description){
+            $parent = null;
+            foreach($childList as &$r){
+                if($name == $r['name']){
+                    $parent = $r['parent'];
+                    unset($r);
+                    $adjacencyListRoles []= ['name' => $name, 'description' => $description, 'parent' => $parent];
+                }
+            }
+            if(!$parent){
+                $adjacencyListRoles []= ['name' => $name, 'description' => $description, 'parent' => $parent];
+            }
+        }
+        $dataProvider = new ArrayDataProvider([
+            'allModels' => $adjacencyListRoles,
+            'sort' => [
+                'attributes' => ['name', 'description'],
+            ],
+            'pagination' => [
+                'pageSize' => 50,
+            ]
+        ]);
+        ?>
+        <?= TreeGrid::widget([
+            'dataProvider' => $dataProvider,
+            'keyColumnName' => 'name',
+            'parentColumnName' => 'parent',
+            'parentRootValue' => null, //first parentId value
+            'pluginOptions' => [
+                //'initialState' => 'collapsed',
+            ],
+            'columns' => [
+                ['attribute' => 'name', 'header' => 'Роль'],
+                ['attribute' => 'description', 'header' => 'Описание'],
+                ['attribute' => 'parent', 'header' => 'Родитель'],
+                ['class' => 'yii\grid\ActionColumn',
+                    'template' => '{update} {delete}',
+                    'buttons' =>
+                        [
+                            'update' => function ($url, $model) {
+                                return Html::a('<span class="glyphicon glyphicon-pencil"></span>', Url::toRoute(['update-role', 'name' => $model['name']]), [
+                                    'title' => Yii::t('yii', 'Update'),
+                                    'data-pjax' => '0',
+                                ]); },
+                            'delete' => function ($url, $model) {
+                                return Html::a('<span class="glyphicon glyphicon-trash"></span>', Url::toRoute(['delete-role','name' => $model['name']]), [
+                                    'title' => Yii::t('yii', 'Delete'),
+                                    'data-confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
+                                    'data-method' => 'post',
+                                    'data-pjax' => '0',
+                                ]);
+                            }
+                        ]
+                ],
+            ]
+        ]) ?>
+    </div>
 </div>
